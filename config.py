e6fa136ad4901d719a8c8e8fd7f1b407ee4c505e
BmartOcho <109639555+BmartOcho@users.noreply.github.com>
2025-06-04 13:43:14 -0500

    updated paths

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 import os
 
 # Folder to watch
-WATCH_FOLDER = r"C:\Users\Benjamin\Desktop\ProofPrinted\Incoming"
+WATCH_FOLDER = r"C:\Users\Benjamin\Desktop\PrePressDigi\watch_folder"
 
 # Path to your Illustrator script template
 SCRIPT_TEMPLATE = os.path.join(os.getcwd(), "Scripts", "check_and_export.jsx")
 
 # Location to copy script that Illustrator auto-loads
-TEMP_SCRIPT = os.path.expandvars(r"%APPDATA%\Adobe\Illustrator Script Runner\run_this.jsx")
+TEMP_SCRIPT = os.path.expandvars(r"%APPDATA%\Roaming\Adobe\Illustrator Script Runner\run_this.jsx")
 
 # Adobe app you want to trigger
 ILLUSTRATOR_EXE = r'"C:\Program Files\Adobe\Adobe Illustrator 2025\Support Files\Contents\Windows\Illustrator.exe"'
